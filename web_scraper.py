1065f0c9b77a49e01906d9d884e81587524d94ad
Madhumitha P Ravishankar <27342390+alzed@users.noreply.github.com>
2021-12-23 01:34:21 +0530

    Use attrs attribute

--- web_scraper.py
+++ web_scraper.py
@@ -3,29 +3,11 @@ from bs4 import BeautifulSoup
 import re
 
 def sanitize(item, filter):
-    if item.has_attr('name'):
-        if not filter:
-            return {
-                "name": item.get('name'),
-                "content": item.get('content')
-            }
-        if re.match(filter+':.*', item.get('name')):
-            return {
-                "name": item.get('name'),
-                "content": item.get('content')
-            } 
+    if item.has_attr('name') and (not filter or re.match(filter+':.*', item.get('name'))):
+        return item.attrs
         
-    if item.has_attr('property'):
-        if not filter:
-            return {
-                "property": item.get('property'),
-                "content": item.get('content')
-            }
-        if re.match(filter+':.*', item.get('property')):
-            return {
-                "property": item.get('property'),
-                "content": item.get('content')
-            }
+    if item.has_attr('property') and (not filter or re.match(filter+':.*', item.get('property'))):
+        return item.attrs
     
 
 def get_meta_data(url, type):
@@ -33,3 +15,4 @@ def get_meta_data(url, type):
     html = BeautifulSoup(response.text, 'html.parser')
     resp = html.find_all('meta')
     return list(filter(lambda a: a is not None, [sanitize(r, type) for r in resp]))
+
